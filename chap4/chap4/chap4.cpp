b48f053628f46d1186185c37a0a139959def5ba6
yuryvn <yury.v.n@gmail.com>
2016-09-11 18:12:05 +0200

    empty project save

---
+++ chap4/chap4/chap4.cpp
@@ -0,0 +1,18 @@
+// chap4.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <time.h>
+
+
+using namespace std;
+
+
+int main()
+{
+	return 0;
+}
+
