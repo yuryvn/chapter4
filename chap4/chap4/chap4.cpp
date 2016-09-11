25f9f80ee86f333bb935ac860a5046b43c48acb6
yuryvn <yury.v.n@gmail.com>
2016-09-11 22:26:27 +0200

    stroka kak massive i ex 4.10

--- chap4/chap4/chap4.cpp
+++ chap4/chap4/chap4.cpp
@@ -10,9 +10,59 @@
 
 using namespace std;
 
+//
+//int main(){
+//	char stroka[20];
+//	int array[10] = { 4, 3, 15 };
+//	cout << "key" << setw(6) << "value" << endl;
+//	for (int i = 0; i < 10; i++){
+//		cout << setw(3)<<i << setw(6) << array[i] << endl;
+//	}
+//	cout << "enter stroku" << endl;
+//	cin >> stroka;
+//	cout << stroka << endl;
+//	for (int i = 0; stroka[i] != '\0'; i++)
+//		cout << stroka[i] << " ";
+//	cout << endl;
+//	return 0;
+//}
 
-int main()
-{
+//------------------------------ex 4.10-----------------------------------------
+
+int main(void){
+	int sellercounter[9] = {};
+	int sellers = 15, key;
+	float soldvalue, proc=0.09;
+
+	cout << "Enter amount of sellers" << endl;
+	cin >> sellers;
+
+	for (int i = 1; i <= sellers; i++){
+		cout << "Enter sold items value for " << i << " seller" << endl;
+		cin >> soldvalue;
+		cout << "salary=" << 200 + soldvalue*proc<<endl;
+		key = (int)(soldvalue*proc / 100);
+		if (key > 8) key = 8;
+		sellercounter[key]++;
+	}
+	cout << "Salary range" << "     " << "number of sellers"<<endl;
+	for (int i = 0; i <= 8; i++){
+		cout << 200 + 100 * i << "-" <<setw(6)<<left<< 200 + 100 * i + 99<<"\t" << right<<setw(4) << sellercounter[i] << endl;
+	}
+
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
